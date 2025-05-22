353c8e7543bf733da916351ea39eb07e5d7c20f6
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2025-05-22 10:52:23 +0600

    done category and sub category

---
+++ src/components/PagesComponents/Manage/SubCategory.tsx
@@ -0,0 +1,84 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Table, TableProps } from "antd";
+import { useState } from "react";
+import { MdOutlineModeEdit } from "react-icons/md";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import EditSubCategoryModal from "./EditSubCategoryModal";
+
+type Idata = {
+    id: number,
+    category: string,
+}
+const SubCategory = () => {
+    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
+    const data: Idata[] = [
+        {
+            id: 1,
+            category: "Electronics",
+        },
+        {
+            id: 2,
+            category: "Plumber",
+        },
+        {
+            id: 3,
+            category: "Cleaner",
+        },
+    ]
+
+    const columns: TableProps<Idata>['columns'] = [
+        {
+            title: "Category",
+            dataIndex: "category",
+            align: 'center',
+            render: (_: any, record: Idata) => <span>{record?.category}</span>,
+        },
+        {
+            title: "Image",
+            dataIndex: "image",
+            align: 'center',
+            render: () =>
+                <div className=" flex justify-center">
+                    <img className="w-20" src="https://metropha.com/wp-content/uploads/2018/09/Metro-Plumbing-_-6-Characteristics-That-An-Emergency-Plumber-In-Chattanooga-TN-Must-Have.jpg" alt="" />
+                </div>,
+        },
+        {
+            title: "Action",
+            dataIndex: "action",
+            align: 'right',
+            render: () =>
+                <div className=" flex justify-end">
+                    <div className=' flex items-center gap-2'>
+                        <button onClick={showModal} className=" bg-primaryColor p-1 rounded cursor-pointer"><MdOutlineModeEdit className="w-6 h-6 text-white" /></button>
+                        <button className=" bg-red-600 p-1 rounded cursor-pointer"><RiDeleteBin6Line className="w-6 h-6 text-white" /></button>
+                    </div>
+                </div>
+
+        },
+
+    ];
+
+    return (
+        <div>
+            <Table
+                columns={columns}
+                className="mt-5 overflow-x-scroll xl:overflow-auto bg-white rounded-lg"
+                dataSource={data}
+                pagination={false}
+                rowKey="_id"
+            />
+            <EditSubCategoryModal isModalOpen={isModalOpen} handleOk={handleOk} handleCancel={handleCancel}></EditSubCategoryModal>
+        </div>
+    );
+};
+
+export default SubCategory;
--- src/pages/manage/Manage.tsx
+++ src/pages/manage/Manage.tsx
@@ -4,9 +4,10 @@ import Materials from "../../components/PagesComponents/Manage/Materials";
 import AddCategoryModal from "../../components/PagesComponents/Manage/AddCategoryModal";
 import { Input } from "antd";
 import { CiSearch } from "react-icons/ci";
+import SubCategory from "../../components/PagesComponents/Manage/SubCategory";
+import AddSubCategoryModal from "../../components/PagesComponents/Manage/AddSubCategoryModal";
 
-
-type Tab = "category" | "materials";
+type Tab = "category" | "sub category" | "questions" | "materials";
 
 const Manage = () => {
     const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
@@ -19,20 +20,30 @@ const Manage = () => {
     const handleCancel = () => {
         setIsModalOpen(false);
     };
+    const [isSubCategoryModalOpen, setIsSubCategoryModalOpen] = useState<boolean>(false);
+    const showSubCategoryModal = () => {
+        setIsSubCategoryModalOpen(true);
+    };
+    const handleSubCategoryOk = () => {
+        setIsSubCategoryModalOpen(false);
+    };
+    const handleSubCategoryCancel = () => {
+        setIsSubCategoryModalOpen(false);
+    };
     const [activeTab, setActiveTab] = useState<Tab>("category");
 
 
     return (
         <div className=" bg-white rounded-md px-3 py-4 min-h-screen">
             <div className=" mb-5">
-                <h2 className="text-md md:text-xl font-semibold ">Subscription</h2>
+                <h2 className="text-md md:text-xl font-semibold ">Manage</h2>
 
             </div>
             <div className=" flex justify-between gap-4">
                 <div className=" flex gap-4">
                     <button
                         onClick={() => setActiveTab("category")}
-                        className={`cursor-pointer px-10 py-3 rounded-md  ${activeTab === "category"
+                        className={`cursor-pointer px-10 py-2 rounded-md  ${activeTab === "category"
                             ? "text-primary bg-primaryColor text-white"
                             : " border border-primaryColor"
                             }`}
@@ -40,8 +51,17 @@ const Manage = () => {
                         Category
                     </button>
                     <button
+                        onClick={() => setActiveTab("sub category")}
+                        className={`cursor-pointer px-10 py-2 rounded-md  ${activeTab === "sub category"
+                            ? "text-primary bg-primaryColor text-white"
+                            : " border border-primaryColor"
+                            }`}
+                    >
+                        Sub Category
+                    </button>
+                    <button
                         onClick={() => setActiveTab("materials")}
-                        className={`cursor-pointer px-10 py-3 rounded-md  ${activeTab === "materials"
+                        className={`cursor-pointer px-10 py-2 rounded-md  ${activeTab === "materials"
                             ? "text-primary bg-primaryColor text-white"
                             : " border border-primaryColor"
                             }`}
@@ -51,15 +71,22 @@ const Manage = () => {
                 </div>
                 <div>
                     {
-                        activeTab === "category" ? <button onClick={showModal} className=" bg-primaryColor py-2 px-4 rounded-md cursor-pointer text-white">+ Add</button> :
-                            <div className=" w-[250px]">
-                                <Input prefix={<CiSearch className=" w-6 h-6" />} className="w-[250px]" placeholder="Search" />
-                            </div>
+                        activeTab === "category" && <button onClick={showModal} className=" bg-primaryColor py-2 px-4 rounded-md cursor-pointer text-white">+ Add</button>
+                    }
+                    {
+                        activeTab === "materials" && <div className=" w-[250px]">
+                            <Input prefix={<CiSearch className=" w-6 h-6" />} className="w-[250px] h-10" placeholder="Search" />
+                        </div>
+                    }
+                    {
+                        activeTab === "sub category" && <button onClick={showSubCategoryModal} className=" bg-primaryColor py-2 px-4 rounded-md cursor-pointer text-white">+ Add</button>
                     }
                 </div>
                 <AddCategoryModal isModalOpen={isModalOpen} handleOk={handleOk} handleCancel={handleCancel}></AddCategoryModal>
+                <AddSubCategoryModal isModalOpen={isSubCategoryModalOpen} handleOk={handleSubCategoryOk} handleCancel={handleSubCategoryCancel}></AddSubCategoryModal>
             </div>
             {activeTab === "category" && <Category />}
+            {activeTab === "sub category" && <SubCategory />}
             {activeTab === "materials" && <Materials />}
         </div>
     );
