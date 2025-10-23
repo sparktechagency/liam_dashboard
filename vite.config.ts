91ff17a8e7abae2eb292b19e77567b3d4e6af44d
goni715 <halesgoni715149123@gmail.com>
2025-10-23 12:54:59 +0600

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
+ 
+ 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-   server: {
+  plugins: [react()],
+  server: {
     //host: '13.53.182.102',  
     host: '0.0.0.0',
-    port: 4173   
+    port: 5173   
+  },
+  preview: { 
+    allowedHosts: ['dashboard.servana.com.au', '16.16.183.92', "www.dashboard.servana.com.au"],  
   },
 })
